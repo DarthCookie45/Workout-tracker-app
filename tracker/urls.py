36052af6b229bacccf1eba0706cfcf7a3a5bceed
Cookie <darthcookienomnom@gmail.com>
2026-05-14 11:50:35 +0100

    Add workout edit and delete capability

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path(
+        'edit/<int:workout_id>/',
+        views.edit_workout,
+        name='edit_workout'
+    ),
+
+    path(
+        'delete/<int:workout_id>/',
+        views.delete_workout,
+        name='delete_workout'
+    ),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -24,3 +24,38 @@ def home(request):
     }
     
     return render(request, 'tracker/home.html', context)
+
+def edit_workout(request, workout_id):
+
+    workout = Workout.objects.get(id=workout_id)
+
+    if request.method == 'POST':
+
+        form = WorkoutForm(request.POST, instance=workout)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    else:
+        form = WorkoutForm(instance=workout)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'tracker/edit_workout.html', context)
+
+def delete_workout(request, workout_id):
+
+    workout = Workout.objects.get(id=workout_id)
+
+    if request.method == 'POST':
+        workout.delete()
+        return redirect('home')
+    
+    context = {
+        'workout': workout
+    }
+
+    return render(request, 'tracker/delete_workout.html', context)
