4daab3dfd1aa6622c1c7a0e7ebdae37b90ba1838
Cookie <darthcookienomnom@gmail.com>
2026-05-22 13:45:19 +0100

    Add custom 404 and 500 error pages

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%#2v*3*h=xf973750-7q9ltrzpr**ybu_r3*ih^o+^_8gr$=40
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
